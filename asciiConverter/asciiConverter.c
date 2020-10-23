b7ff59789ba44af13b4fe52732113f4268aae2c1
Adam20058 <adamjemal93@gmail.com>
2020-10-22 23:53:47 -0400

    ASCII converter application: Coverts text into ASCII number format

---
+++ asciiConverter/asciiConverter.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+	printf("\n");
+	printf("************************\n");
+	printf("****ASCII Converter!****\n");
+	printf("************************\n");
+	printf("\n");	
+
+	char ASCII;
+	printf("Enter a character: ");	
+	scanf("%c", &ASCII);	
+	printf("\n");	
+	printf("%d\n", ASCII);	
+	printf("\n");	
+
+	return 0;
+}
