3dff8623fdf9e7473d74f55f4adb8bb7229af2c4
Adam20058 <adamjemal93@gmail.com>
2020-11-03 14:04:48 -0500

    revision arithmetic expressions: shorthand notation

--- artExp2/aexp2.c
+++ artExp2/aexp2.c
@@ -32,6 +32,45 @@ int main(void)
   printf("\n");
   c = d + 0.5;
   printf("if you round up d you'll get %d\n", c);
+  printf("\n");
+
+  // Another thing to consider
+  // To convert intergers to floating point values. We have 2 different ways of accomplishing this:
+  // 1. Use mix mode expression ->
+  // (a) By casting one of the integers you'll be able to divide it
+  // (b) By adding a decimal point to the value itself you can divide it as well
+
+  double test;
+
+  test = (double)4 / 5;
+  printf("test is %0.2lf \n", test);
+  printf("\n");
+
+  d = 2 / 3;
+  printf("d is %0.2lf \n", d);
+  printf("\n");
+
+  // In summary both methods helps you retain the fraction ... prevents the computer from losing it
+
+  // some shortcuts to take in mind.... lets compare long form and short form
+
+  //LONG WAY ---> alot of people do operations like this by taking the value of the variable itself and do something to it and then assign it to itself.... so they modify it
+  // how ever there is a shorter way of doing this
+
+  b = 5;
+  b = b * 2;
+
+  printf("%d\n", b);
+  printf("\n");
+
+  // shortcut notation
+  // you simply combine the operator with equal....
+  // (e.g) b *= 2, this would translate to b is equal to b multiplied by 2
+  // (e.g) c -=2, this would translate to b is equal to b subtract by 2
+
+  b /= 2;
+  printf("%d\n", b);
+  printf("\n");
 
   return 0;
 }
