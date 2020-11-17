28e35020e483556739817a092c9968b4dc7c28b2
Adam20058 <adamjemal93@gmail.com>
2020-11-17 13:23:06 -0500

    created a program that finds prime had to debug it but now it works

---
+++ debugDemo/README.md
@@ -0,0 +1,26 @@
+# bugDemo
+
+bugDemo is a program finds the prime numbers between 0 to 100
+
+# What is a prime?
+
+Prime is a number that is divisible only 1 and itself
+So basically primes are numbers that have only 2 factors, 1 and themselves.
+
+# Who cares?
+
+Ever heard of astrophysicist extraordinaire Carl Sagan, you know the host of the most awesome tv show "Cosmos". In his science fiction novel "Contact" he describes the prime to be one of the fundamental building blocks of all numbers.
+
+Insects such as Cicadas time their life cycles by them.
+
+# Sorry how does it impact me?
+
+Still not convinced....
+
+Oh boy, well I would like to inform you they play an important role in our world, infact you're using prime numbers in your every day lives.
+
+They play a fundamental role in computing specifically in security and encryption.
+
+Ever use a credit card or debit card on Amazon. Well if you do, you should know prime numbers play an important role in encryption and decryption of the transaction process between you and the merchant your buying from.
+
+In summary prime numbers are awesome and you should definitely watch "Cosmos".
---
+++ debugDemo/bugDemo.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+//*** this program finds the prime numbers between 0 to 10 ***
+
+//*** Well what is a prime? ****
+
+//Prime is a number that is divisible only 1 and itself
+//Basically primes are numbers that have only 2 factors -> 1 and themselves.
+
+int main(void)
+{
+  int i, j, prime = 0;
+
+  for (i = 0; i < 100; i++)
+  {
+    prime = 1;
+
+    for (j = i - 1; j > 1; j--) // subtracted the i variable by 1. To prevent J from dividing from it self in the if condition nested inside
+    {
+      //printf("%d mod %d = %d \n", i, j, i % j); // added a printf statement to see what were the updates on the variable values
+      if (i % j == 0) //added curly braces for all the nested elements just to make it easy to follow
+      {
+        prime = 0;
+      }
+    }
+    if (prime) // added curly braces for nested elements just for easy read
+    {
+      if (i > 1) // added this to remove 0 and 1. Since they're not considered prime numbers
+      {
+        printf("%d is prime \n", i);
+      }
+    }
+  }
+
+  return 0;
+}
+
+/*int main(void)
+{
+  int i, j, prime = 0;
+
+  for (i = 0; i < 10; i++)
+  {
+    prime = 1;
+    for (j = i; j > 1; j--)
+    {
+      if (i % j == 0)
+        prime = 0;
+    }
+    if (prime)
+      printf("%d is prime \n", i);
+  }
+
+  return 0;
+}
+*/
